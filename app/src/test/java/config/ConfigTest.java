f3a38bbff22e2dfd6fcdfd167b81fd30c405ec71
haryoiro <mizusecocolte@gmail.com>
2023-09-17 13:23:31 +0900

    💚 テストコード追加

---
+++ app/src/test/java/config/ConfigTest.java
@@ -0,0 +1,28 @@
+package config;
+
+import calcparser.config.FormatOption;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigTest {
+
+    @Test
+
+    void test() {
+        // Arrange
+        FormatOption.Option option = new FormatOption.Option();
+        option.setAddParenthesis(false);
+        option.setSpaceAroundParenthesis(false);
+        option.setNewLineAfterParenthesis(false);
+        option.setSpaceAroundOperator(false);
+        FormatOption expectOption = new FormatOption();
+        expectOption.setOption(option);
+
+        // Act
+        FormatOption formatOption = new FormatOption().fromPath("src/test/resources/config/config.toml");
+
+        // Assert
+        assertThat(formatOption).isEqualTo(expectOption);
+    }
+}
