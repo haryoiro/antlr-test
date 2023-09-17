38fe2b30c71a7f4fcd1c8fa29f1f50d8841a578d
haryoiro <mizusecocolte@gmail.com>
2023-09-17 19:37:06 +0900

    👕 フォーマット

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,6 @@ dependencies {
     // logging
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-
-
-
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -73,9 +70,11 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
+tasks {
 
-tasks.generateGrammarSource {
-    outputDirectory = file("src/main/java/com/haryoiro/calcformat/antlr")
-    maxHeapSize = "64m"
-    arguments = arguments + listOf("-visitor", "-long-messages")
+    generateGrammarSource {
+        outputDirectory = file("src/main/java/com/haryoiro/calcformat/antlr")
+        maxHeapSize = "64m"
+        arguments = arguments + listOf("-visitor", "-long-messages")
+    }
 }
