9c1abacf146aeaf7629e5a99c39360de239af6df
haryoiro <mizusecocolte@gmail.com>
2023-09-17 15:37:51 +0900

    [WIP]

--- app/src/main/java/com/haryoiro/calcformat/formatting/CalcFormatVisitor.java
+++ app/src/main/java/com/haryoiro/calcformat/formatting/CalcFormatVisitor.java
@@ -11,26 +11,67 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import java.util.List;
 import java.util.function.Function;
 
+import static com.google.common.base.Ascii.SPACE;
+
 @RequiredArgsConstructor
 public class CalcFormatVisitor extends CalcBaseVisitor<String> {
 
     private final FormatOption option;
 
+    private int level = 0;
+    private String indent() {
+        return "  ".repeat(level);  // 2スペースでインデント
+    }
+
     private String joinExpressions(List<? extends ParserRuleContext> expressions,
             List<? extends TerminalNode> operators,
-            Function<ParserRuleContext, String> visitorMethod) {
+            Function<ParserRuleContext, String> visitorMethod, boolean addParentheses) {
         StringBuilder sb = new StringBuilder();
+
+        if (getOption().isAddParenthesis() && addParentheses) {
+            sb.append(spaceAroundParentheses("("));
+            if (getOption().isNewLineAfterParenthesis()) {
+                sb.append("\n");
+            }
+        }
+        if (getOption().isAddParenthesis() && getOption().isNewLineAfterParenthesis()) {
+            sb.append(indent());
+        }
+
         sb.append(visitorMethod.apply(expressions.get(0)));
+
         for (int i = 1; i < expressions.size(); i++) {
-            sb.append(" ").append(operators.get(i - 1).getText()).append(" ");
+
+            if (getOption().isSpaceAroundOperator()) {
+                sb.append(" ");
+            }
+            sb.append(operators.get(i - 1).getText());
+            if (getOption().isSpaceAroundOperator()) {
+                sb.append(" ");
+            }
+
+            if (getOption().isNewLineAfterParenthesis()) {
+                sb.append("\n").append(indent());
+            }
+
             sb.append(visitorMethod.apply(expressions.get(i)));
         }
+
+        if (getOption().isAddParenthesis() && addParentheses) {
+            if (getOption().isNewLineAfterParenthesis()) {
+                sb.append("\n").append(indent());
+            }
+            sb.append(spaceAroundParentheses(")"));
+        }
+
         return sb.toString();
     }
 
     @Override
     public String visitStart(CalcParser.StartContext ctx) {
-        return visit(ctx.expr()) + "\n";
+        String origin = visit(ctx.expr());
+
+        return origin + "\n";
     }
 
     @Override
@@ -40,42 +81,54 @@ public class CalcFormatVisitor extends CalcBaseVisitor<String> {
 
     @Override
     public String visitAdd_expr(CalcParser.Add_exprContext ctx) {
-        return wrapParentheses(joinExpressions(ctx.mul_expr(), ctx.PLUS_MINUS(), this::visit));
+        return joinExpressions(ctx.mul_expr(), ctx.PLUS_MINUS(), this::visit,
+                ctx.mul_expr().size() > 1);
     }
 
     @Override
     public String visitMul_expr(CalcParser.Mul_exprContext ctx) {
-        return wrapParentheses(joinExpressions(ctx.atom(), ctx.MUL_DIV(), this::visit));
+        return joinExpressions(ctx.atom(), ctx.MUL_DIV(), this::visit, ctx.atom().size() > 1);
     }
-
 
     @Override
     public String visitAtom(CalcParser.AtomContext ctx) {
+        StringBuilder sb = new StringBuilder();
+
         if (ctx.NUMBER() != null) {
-            return ctx.NUMBER().getText();
+            sb.append(ctx.NUMBER().getText());
         }
         if (ctx.IDENTIFIER() != null) {
-            return ctx.IDENTIFIER().getText();
+            sb.append(ctx.IDENTIFIER().getText());
         }
         if (ctx.expr() != null) {
-            return wrapParentheses(visit(ctx.expr()));
+            var st = visit(ctx.expr());
+            sb.append(st);
         }
-        throw new RuntimeException("Unknown atom: " + ctx.getText());
+
+        return sb.toString();
     }
 
     private Option getOption() {
         return option.getOption();
     }
 
-    private String wrapParentheses(String expr) {
-        StringBuilder sb = new StringBuilder();
+    private String spaceAroundParentheses(String expr) {
+        if (getOption().isSpaceAroundParenthesis()) {
+            StringBuilder sb = new StringBuilder();
 
-        if (getOption().isAddParenthesis()) {
-            sb.append("(");
-            sb.append(expr);
-            sb.append(")");
+            if (expr.startsWith("(")) {
+                sb.append("(");
+                sb.append(" ");
+            } else if (expr.endsWith(")")) {
+                if (!getOption().isNewLineAfterParenthesis()) {
+                    sb.append(" ");
+                }
+                sb.append(")");
+            }
+
             return sb.toString();
         }
+
         return expr;
     }
 
