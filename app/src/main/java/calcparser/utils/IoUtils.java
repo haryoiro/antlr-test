db106ce7c5ae4460d97730d4a9d2c1f8dc2da9a1
haryoiro <mizusecocolte@gmail.com>
2023-09-17 12:45:33 +0900

    🚑 バッファをうまく取れないのを修正

--- app/src/main/java/calcparser/utils/IoUtils.java
+++ app/src/main/java/calcparser/utils/IoUtils.java
@@ -1,4 +1,4 @@
-package calcparser.config;
+package calcparser.utils;
 
 import java.io.File;
 import java.io.IOException;
@@ -6,23 +6,23 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class IoUtils {
 
     public static String readFile(File f, int bufSize) {
+        System.out.println(f.toURI());
         try (ReadableByteChannel rbc = FileChannel.open(Paths.get(f.toURI()),
                 StandardOpenOption.READ)) {
-            char[] ca = new char[bufSize];
             ByteBuffer bb = ByteBuffer.allocate(bufSize);
             StringBuilder sb = new StringBuilder();
             while (rbc.read(bb) > -1) {
-                CharBuffer cb = bb.asCharBuffer();
-                cb.flip();
-                cb.get(ca);
-                sb.append(ca);
-                cb.clear();
+                bb.flip();  // バッファを読み取りモード
+                CharBuffer cb = StandardCharsets.UTF_8.decode(bb);
+                sb.append(cb);
+                bb.clear();  // バッファをクリアして次の読み取りモードへ
             }
             return sb.toString();
         } catch (IOException e) {
